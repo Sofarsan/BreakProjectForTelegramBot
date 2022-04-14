32fd3a7cad256cc4efb7c6977d7fa979eff0b75c
Diash177 <Diash177@gmail.com>
2022-04-14 13:50:44 +0500

    Add User Class

---
+++ BusinessLogicLayer/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class User
+    {
+        string _lastName;
+        string _name;
+        int _age;
+
+        public User (string lastName, string name, int age)
+        {
+            _lastName=lastName;
+            _name=name;
+            _age=age;
+        }
+    }
+}
