6fe2a5bcc2281eaa86d63cf122a76a830b695c9e
Eugen <phlosof@gmail.com>
2022-04-18 23:39:58 +0300

    delete HEAD

---
+++ BusinessLogicLayer/Telega.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+
+namespace BusinessLogicLayer
+{
+    public class Telega
+    {
+        TelegramBotClient client = new TelegramBotClient("5331081992:AAEmEzmU2lWqKLn9mgYCYbcNnPSLVDEHHQM");
+
+    }
+}
