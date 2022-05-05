d37fb5d6e23612c74880d331ce545f9e8d792e8b
Diash177 <Diash177@gmail.com>
2022-05-06 02:47:18 +0500

    Add groups serializer

---
+++ BusinessLogicLayer/Telegram/GroupsSerialize.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace BusinessLogicLayer.GroupsSerialize
+{
+    public static class GroupsSerialize
+    {
+        private const string groupsDictionaryJson = @"Groups.json";
+        public static string GroupsObservableCollectionSerialize(ObservableCollection<UserGroup> groups)
+        {
+            return JsonSerializer.Serialize<ObservableCollection<UserGroup>>(groups);
+        }
+
+        public static ObservableCollection<UserGroup> GroupsObservableCollectionDecerialize(string json)
+        {
+            if (json == null)
+            {
+                throw new ArgumentNullException(groupsDictionaryJson);
+            }
+            else
+            {
+                return JsonSerializer.Deserialize<ObservableCollection<UserGroup>>(json);
+            }
+        }
+
+        public static void SaveTestsObservableCollection(ObservableCollection<UserGroup> groups)
+        {
+            string json = GroupsObservableCollectionSerialize(groups);
+
+            using (StreamWriter sw = new StreamWriter(groupsDictionaryJson, false))
+            {
+                sw.WriteLine(json);
+            }
+        }
+
+        public static ObservableCollection<UserGroup> LoadGroupsObservableCollectionDecerialize()
+        {
+            if (File.Exists(groupsDictionaryJson))
+            {
+                using (StreamReader sr = new StreamReader(groupsDictionaryJson))
+                {
+                    string json = sr.ReadLine();
+
+                    return GroupsObservableCollectionDecerialize(json);
+
+                }
+            }
+            else
+            {
+                return new ObservableCollection<UserGroup>();
+            }
+        }
+    }
+}
+
